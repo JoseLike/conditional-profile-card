09d6e6007e9c3c8a914b5caef712c02d7c5ed669
JoseLike <joseluisgil94@gmail.com>
2022-03-19 11:50:48 +0000

    enlazados inputs, arreglar nulls

--- src/js/app.js
+++ src/js/app.js
@@ -29,18 +29,43 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
+  let apellido = variables.lastname == null ? `` : `${variables.lastname}`;
+
+  let nombre =
+    variables.name == null
+      ? `<h1>Seleccione nombre</h1>`
+      : `<h1>${variables.name}</h1>`;
+
+  let puesto =
+    variables.role == null ? `<h2></h2>` : `<h2>${variables.role}</h2>`;
+
+  let localizacion =
+    variables.city == null && variables.country == null
+      ? `<h3>Seleccione nombre</h3>`
+      : `<h3>${variables.city},${variables.country}</h3>`;
+
+  let posicion =
+    variables.socialMediaPosition == "position-left"
+      ? `class="position-left"`
+      : `class="position-right"`;
+
+  let atwitter = variables.twitter;
+  let agithub = variables.github;
+  let alinkedin = variables.linkedin;
+  let ainstagram = variables.instagram;
+
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            ${nombre}
+            ${puesto}
+            ${localizacion}
+          <ul ${posicion}>
+            <li><a href=${atwitter}><i class="fab fa-twitter"></i></a></li>
+            <li><a href=${agithub}><i class="fab fa-github"></i></a></li>
+            <li><a href=${alinkedin}><i class="fab fa-linkedin"></i></a></li>
+            <li><a href=${ainstagram}><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
